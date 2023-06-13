04565981c502a7afc39dcfffcc4f3411c70b0eeb
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-13 16:31:52 +0200

    added extra bonus to check that there is no duplicate email

--- js/myscript.js
+++ js/myscript.js
@@ -9,18 +9,44 @@ createApp ({
 
     methods: {
         
+        // ---Versione che fa richieste finchè non abbiamo 10 email diverse---
+        async randomEmails(){
+            for(let i= 0; i<20; i++){
+                // ---Aspetto la risposta dal server---
+                await axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then( (response) => {
+                    const result = response.data.response;
+                    // ---Pusho l'email solo se diversa da quelle già presenti nell'array---     
+                    if(!this.emails.includes(result)){
+                        this.emails.push(result)
+                    }
+                    // ---Arrivato a 10 email diverse nell'array fermo il ciclo for---
+                    if(this.emails.length >= 10){
+                        i=19
+                    }
+                });
+            }
+        },
+
+        // ---Versione che fa 20 richieste ma ne pusha solo 10 nell'array e le altre 10 le butta---
+        // async randomEmails(){
+        //     for(let i= 0; i<20; i++){
+        //         // ---Aspetto la risposta dal server---
+        //         await axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        //         .then( (response) => {
+        //             const result = response.data.response;       
+        //             // ---Pusho l'email solo se diversa da quelle già presenti nell'array e la lunghezza dell'array è inferiore a 10---
+        //             if(!this.emails.includes(result) && this.emails.length < 10){
+        //                 this.emails.push(result)
+        //             }
+        //         });
+        //     }
+        // },
+        
     },
 
     created(){
-        // ? quando l'applicazione è stata creata e ancora non è montata
-        // this.getNewRandomNumber();
-        for(let i= 0; i<10; i++){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then( (response) => {
-            // § se e quando il server risponde
-            const result = response.data.response;
-            this.emails.push(result)
-        });
-        }
+        this.randomEmails()
     },
+
 }).mount ("#app")
